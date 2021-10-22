a075859e65778a06529beeefe1108b7dbd681028
tramtran0497 <tramtran0497@gmail.com>
2021-10-22 13:06:39 +0300

    Done part1 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,7 @@ const Button = ({onClick, text})=>{
 
 const StatisticLine = ({text, value})=>{
   return(
-    <p>{text} {value}</p>
+      <p>{text} {value}</p> 
   )
 }
 
@@ -21,12 +21,40 @@ const Statistics = ({good,neutral,bad,all,ave,pos})=>{
   } 
   return(
     <div>
-      <StatisticLine text='Good' value={good} />
-      <StatisticLine text='Neutral' value={neutral} />
-      <StatisticLine text='Bad' value={bad} />
-      <StatisticLine text='All' value={all} />
-      <StatisticLine text='Average' value={ave} />
-      <StatisticLine text='Positive' value={pos} />
+      <table>
+        <tr>
+          <td><StatisticLine text='Good' /></td>
+          <td><StatisticLine value={good} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text='Neutral' /></td>
+          <td><StatisticLine value={neutral} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text='Bad' /></td>
+          <td><StatisticLine value={bad} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text='All' /></td>
+          <td><StatisticLine value={all} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text='Average' /></td>
+          <td><StatisticLine value={ave} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text='Positive' /></td>
+          <td><StatisticLine value={pos} /></td>
+          <td>%</td>
+        </tr>
+          {/* <StatisticLine text='Good' value={good} />
+          <StatisticLine text='Neutral' value={neutral} />
+          <StatisticLine text='Bad' value={bad} />
+          <StatisticLine text='All' value={all} />
+          <StatisticLine text='Average' value={ave} />
+          <StatisticLine text='Positive' value={pos} /> */}
+      </table>
+     
     </div>
     
   )
@@ -40,8 +68,9 @@ const App = ()=>{
   
   
   const all = good + neutral + bad
-  const ave = (good + bad * -1)/all
-  const pos = (good/all)*100
+  const ave = Math.round(((good + bad * -1)/all)*10)/10
+  const pos = ((good/all)*100).toFixed(1)
+  
 
   return(
     <div>
