7d26fc8685cbf58b9927a30fc39abd248848fce5
tramtran0497 <tramtran0497@gmail.com>
2021-10-23 14:02:19 +0300

    Done part1 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () =>{
   const anecdotes = [
@@ -10,22 +10,6 @@ const App = () =>{
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
   'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-  // const point = [
-  //   0,0,0,0,0,0,0
-  // ]
-  // const point = {
-  //   0:1,
-  //   1:3,
-  //   2:4,
-  //   3:2,
-  //   4:1,
-  //   5:3,
-  //   6:2
-  // }
-  // const copy = {...point}
-  // const handleVote = ()=>{
-  //   return copy[selected]+=1
-  // }
   const [point, setPoint] = useState({
     0:1,
     1:3,
@@ -33,26 +17,50 @@ const App = () =>{
     3:2,
     4:1,
     5:3,
-    6:2
+    6:9
   })
+  const [maxVotePos, setMaxVotePos] = useState(0);
   const handleVote = ()=>{
     const newValue = {
       ...point,
-      [selected]: point[selected]+1,
-      // 0: point[0] + 1
-    }
+      [selected]: point[selected]+1
+    } 
     setPoint(newValue)
+
+    const maxPos = findMaxVotePos()
+    setMaxVotePos(maxPos)
+
   }
   const [selected, setSelected] = useState(0)
   const random = Math.round(Math.random()*6)
-  console.log(random,selected,point)
-  
+
+  useEffect(() => {
+    const maxPos = findMaxVotePos()
+    setMaxVotePos(maxPos)  
+  })
+  const findMaxVotePos = () => {
+    const _newArr = Object.entries(point)
+    let _maxPos = 0
+    let _maxVote = _newArr[_maxPos][1]
+    // let num = 0
+    for(let i = 1; i < _newArr.length; i++){
+      if(_newArr[i][1] > _maxVote){
+        _maxVote = _newArr[i][1]
+        _maxPos = i
+      }  
+    }
+    return _maxPos;
+  }
   return(
     <div>
+      <h4>Anecdote of tha day</h4>
       <p>{anecdotes[selected]}</p>
       <p>has {point[selected]} votes </p>
       <button onClick={handleVote}>vote</button>
       <button onClick={()=> setSelected(random)}>next anecdotes</button>
+      <h4>Anecdote with most votes</h4>
+      <p>{anecdotes[maxVotePos]}</p>
+      <p>has {point[maxVotePos]} votes</p>
     </div>
   )
 }
