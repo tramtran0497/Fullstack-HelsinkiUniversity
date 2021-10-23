96d4fff438758a354385ae464315c77c920a1c34
tramtran0497 <tramtran0497@gmail.com>
2021-10-23 12:29:39 +0300

    Done part1 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,48 @@ const App = () =>{
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
   'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-
+  // const point = [
+  //   0,0,0,0,0,0,0
+  // ]
+  // const point = {
+  //   0:1,
+  //   1:3,
+  //   2:4,
+  //   3:2,
+  //   4:1,
+  //   5:3,
+  //   6:2
+  // }
+  // const copy = {...point}
+  // const handleVote = ()=>{
+  //   return copy[selected]+=1
+  // }
+  const [point, setPoint] = useState({
+    0:1,
+    1:3,
+    2:4,
+    3:2,
+    4:1,
+    5:3,
+    6:2
+  })
+  const handleVote = ()=>{
+    const newValue = {
+      ...point,
+      [selected]: point[selected]+1,
+      // 0: point[0] + 1
+    }
+    setPoint(newValue)
+  }
   const [selected, setSelected] = useState(0)
-  const random = Math.round(Math.random()*7)
+  const random = Math.round(Math.random()*6)
+  console.log(random,selected,point)
+  
   return(
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {point[selected]} votes </p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={()=> setSelected(random)}>next anecdotes</button>
     </div>
   )
